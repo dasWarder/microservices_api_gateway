69934ffb80fc06ad26821d9e75c5ba3b4eea123a
dasWarder <Al.94_94@mail.ru>
2021-10-11 15:44:57 +0300

    Add Spring Cloud and Reactive web dependencies and configure gateway for automating location

---
+++ src/test/java/com/example/microservices_cloudgateway/MicroservicesCloudGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.microservices_cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesCloudGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
