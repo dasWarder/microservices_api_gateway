d456b40477717b3017a2857d92b6448315f2538f
dasWarder <Al.94_94@mail.ru>
2021-10-19 11:40:53 +0300

    Move logic from CustomGlobalFilter class into GlobalFiltersConfiguration, add more filter beans and annotate them with @Order annotation for ordering

--- src/main/java/com/example/microservices_cloudgateway/filters/GlobalFiltersConfiguration.java
+++ src/main/java/com/example/microservices_cloudgateway/filters/GlobalFiltersConfiguration.java
@@ -4,18 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import reactor.core.publisher.Mono;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Set;
 
 @Slf4j
 @Configuration
 public class GlobalFiltersConfiguration {
 
     @Bean
-    public GlobalFilter globalPrePostFilter() {
+    @Order(2)
+    public GlobalFilter globalPrePostLoggingFilter() {
 
         return (exchange, chain) -> {
 
@@ -30,6 +32,51 @@ public class GlobalFiltersConfiguration {
                 String uri = exchange.getRequest().getURI().toString();
 
                 log.info("The request URI: {}", uri);
+            }));
+        };
+    }
+
+    @Bean
+    @Order(1)
+    public GlobalFilter globalHeaderPreFilter() {
+
+        return ((exchange, chain) -> {
+            log.info("Pre-filter execute");
+
+            HttpHeaders headers = exchange.getRequest().getHeaders();
+            Set<String> keys = headers.keySet();
+
+            keys.forEach(k -> {
+                String value = headers.getFirst(k);
+                log.info("Header key: {}. Header value: {}", k, value);
+            });
+
+            return chain.filter(exchange);
+        });
+    }
+
+    @Bean
+    @Order(3)
+    public GlobalFilter globalFilter() {
+
+        return (exchange, chain) -> {
+
+            String id = exchange.getRequest().getId();
+
+            log.info("Request id: {}", id);
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+
+                HttpStatus statusCode = exchange.getResponse().getStatusCode();
+                Set<String> keys = exchange.getResponse().getHeaders().keySet();
+
+                keys.forEach(k -> {
+                    String value = exchange.getResponse().getHeaders().getFirst(k);
+
+                    log.info("Response key: {} with a value: {}", k, value);
+                });
+
+                log.info("Response status code: {}", statusCode.value());
             }));
         };
     }
